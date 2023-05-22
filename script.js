9b97a07adf8cfccd20f1195f5ae3d9dc2f538042
sonia jaiswal <118751361+soniaj27@users.noreply.github.com>
2023-05-22 09:26:55 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,49 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  getName() {
+    return this.name;
+  set age(age) {
+    this._age = age;
+  }
+setAge(age) {
+    this.age = age;
+
+  get age() {
+    return this._age;
+  }
+}
+
+class Student extends Person {
+	study(){
+	 console.log(this.name +"is studying");
+	}
+  study() {
+    console.log(`${this._name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+	teach(){
+		 console.log(this.name+ "is teaching");
+	}
+  teach() {
+    console.log(`${this._name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
-window.Teacher = Teacher;
